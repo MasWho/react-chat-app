4b8d90334ff702ee952e27c782e6c0ab8c488dca
MasonWho <mason@syftanalytics.com>
2022-06-01 23:29:33 +0200

    Squashed commit of the following:
    commit f64e0c534d70fe4309ffd83d362bc5d48d265cfb
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Wed Jun 1 23:27:30 2022 +0200
    
        setup cors for the express server so that we can accept incoming traffic from any origin for now
    
    commit 988fac5c355358079089bb3b71d67ad1b194f6ba
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Wed Jun 1 23:20:22 2022 +0200
    
        install client-side socket.io and setup component with socket
    
    commit e39300c458045f7d33c579e6c7661f1820e3da24
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Wed Jun 1 23:09:24 2022 +0200
    
        import socketio and create an io object then setup a connection event
    
    commit 065b714559ffeaab8a65b2cc560045e424183ec2
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Wed Jun 1 23:04:42 2022 +0200
    
        refactor to createServer with http and passing in app object created with express
    
    commit 0e281a75b82f4f9540647d4763637901285241d0
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Wed Jun 1 23:00:53 2022 +0200
    
        installed socket.io
    
    commit f7789eccf7099ae49b82a98c0a296c399e212cf8
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Wed Jun 1 22:48:43 2022 +0200
    
        made start script
    
    commit 739348f4e01f11a925f8aa241591a6c84625fbd4
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Wed Jun 1 22:47:50 2022 +0200
    
        install express and setup express server

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+const port = process.env.PORT || 8000;
+
+io.on('connection', () => {
+  console.log('New Connection!');
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import io from 'socket.io-client';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(`http://${window.location.hostname}:8000`);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, []);
+
   return (
     <div className="App">
+      Chat App
     </div>
   );
 }
