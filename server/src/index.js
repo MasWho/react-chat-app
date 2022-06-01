065b714559ffeaab8a65b2cc560045e424183ec2
MasonWho <mason@syftanalytics.com>
2022-06-01 23:04:42 +0200

    refactor to createServer with http and passing in app object created with express

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
