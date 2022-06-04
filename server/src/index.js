f3b9fa9138d14911b5aea92f97fa8fce485d326b
MasonWho <mason@syftanalytics.com>
2022-06-04 16:33:43 +0200

    Squashed commit of the following:
    commit 72bf2276ee149b85dc677a167fdd65b2f70e4263
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Thu Jun 2 23:20:08 2022 +0200
    
        styling
    
    commit fd4aeadfc7f8a02309429ad8d30d37971d51a547
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Thu Jun 2 23:06:03 2022 +0200
    
        setup broadcasting and disconnect event
    
    commit f66990151e69bee1d84a43f602e36adfe000ad24
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Thu Jun 2 22:55:52 2022 +0200
    
        setup input form on client to forward message to all connected clients
    
    commit 7ddd165d8dfccd49c8207ccbde86c1b6f002dcad
    Author: MasonWho <mason@syftanalytics.com>
    Date:   Thu Jun 2 22:34:45 2022 +0200
    
        removed React.StrictMode for dev purposes

--- server/src/index.js
+++ server/src/index.js
@@ -13,8 +13,22 @@ const io = socketio(server, {
 
 const port = process.env.PORT || 8000;
 
-io.on('connection', () => {
-  console.log('New Connection!');
+io.on('connection', (socket) => {
+  // Tell the specific client welcome
+  socket.emit('message', 'Welcome');
+
+  // Tell all other clients the current client joined
+  socket.broadcast.emit('message', 'A new user joined!');
+  
+  // Forward message to all connected clients
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  // This is built-in socket.io event, all other clients should get message that current client disconnected
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!');
+  });
 });
 
 server.listen(port, () => {
