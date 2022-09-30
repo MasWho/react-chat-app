d8210aa760997e6c364a678947445a46cffa6ab3
Spiderpig1998 <jburns@syftanalytics.com>
2022-09-30 14:41:51 +0200

    Updates

--- server/src/redisClient.js
+++ server/src/redisClient.js
@@ -1,5 +1,5 @@
 const {createClient} = require('redis'); 
-const redisClient = createClient();
+const redisClient = createClient({url: 'redis://redis:6379'});
 redisClient.connect();
 
 module.exports = {redisClient};
